a985b6dc427c9246c82d63f39e18f058a32f9c57
kavya1831 <kkm18311@gmail.com>
2019-07-28 21:32:20 +0530

    src package added

--- Project/urls.py
+++ Project/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
-from django.contrib import admin
+from src.views.userview import UserView
 
 # import testview from sample_app/view
 from asset_mt.views import test_view
 from asset_mt.views import asset_db_test
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^testview/', test_view),
-    url(r'^asset/db/view/', asset_db_test),
+    url(r'^user/', UserView.as_view()),
 ]
